16deaa0e2c6fe98905fb61404ed021e0461d4c70
Surya <suryatk2007@gmail.com>
2025-12-04 15:27:44 +0530

    implemented basic operations in map in js

--- html/script.js
+++ html/script.js
@@ -1,18 +1,17 @@
-arr=["Surya","Vikram","Kiran","Ravi","Ajay"];
-console.log(arr);
-arr.push("Suresh");
-console.log(arr);
-arr.pop();
-console.log(arr);
-arr.shift(); // removes first element
-console.log(arr);
-arr.unshift("Mahesh"); // adds element at first position
-console.log(arr);
-// delete arr[2]; // deletes element but keeps index
-// console.log(arr);
-arr.splice(0,2); // removes 2 elements from index 0
-console.log(arr);
-let arr1=arr.slice(0,1); // creates new array from index 0 to 1 (not including 1)
-console.log(arr1);
-arr.splice(0,2,"Ramesh","Suresh"); // adds elements at index 0 by removing 2 elements
-console.log(arr);
+mobile={brand:"Apple", model:"iPhone 12", os:"iOS 14"};
+console.log(mobile);
+console.log(mobile.brand);
+mobile.model="iPhone 13";
+console.log(mobile.model);
+delete mobile.os;
+console.log(mobile);
+newMobile=Object.assign(mobile);
+console.log(newMobile); 
+let keys=Object.keys(mobile);
+console.log(keys);
+let values=Object.values(mobile);
+console.log(values);
+let entries=Object.entries(mobile); 
+console.log(entries);
+let has=mobile.hasOwnProperty("brand");
+console.log(has);   
