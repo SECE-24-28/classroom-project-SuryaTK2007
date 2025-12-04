9b196575ca82b404b968ffe109ded1796732e45c
Surya <suryatk2007@gmail.com>
2025-12-04 16:49:31 +0530

    implemented some and every for an array of objects

--- html/script.js
+++ html/script.js
@@ -1,20 +1,28 @@
 let students = [
     { name: "Surya", age: 20, marks: 85 },
-    { name: "Yaazh", age: 22, marks: 90 },
+    { name: "Yaazh", age: 22, marks: 30 },
     { name: "kevin", age: 23, marks: 88 }
 ];
-console.log(students);
+// console.log(students);
 
-students.forEach(student => {
-    console.log(`Name: ${student.name}, Age: ${student.age}`);
-});
-students.push({ name: "Arun", age: 21 });
-console.log(students);
-students.pop();
-console.log(students);
-students.map(student => {
-    return student.marks += 5;
-}); 
-console.log(students);
-students.filter(student => student.marks > 90);
-console.log(students);
+// students.forEach(student => {
+//     console.log(`Name: ${student.name}, Age: ${student.age}`);
+// });
+// students.push({ name: "Arun", age: 21 });
+// console.log(students);
+// students.pop();
+// console.log(students);
+// students.map(student => {
+//     return student.marks += 5;
+// }); 
+// console.log(students);
+// students.filter(student => student.marks > 90);
+// console.log(students);
+let Stu1=students.every(
+    student => student.marks > 50
+)
+console.log(Stu1);
+let Stu2=students.some(
+    student => student.marks > 50
+)
+console.log(Stu2);
