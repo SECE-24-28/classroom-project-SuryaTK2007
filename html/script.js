6564509fa9dda23f44aef8ba45136b8757c498bf
Surya <suryatk2007@gmail.com>
2025-12-04 15:47:44 +0530

    implemented forEach, push, pop, map and filter in object

--- html/script.js
+++ html/script.js
@@ -1,10 +1,20 @@
-let students = {name: "Surya", age: 18, demo:function(){
-    console.log("Hello "+this.name);
-}};
-console.log(students.name);
-students.demo();
+let students = [
+    { name: "Surya", age: 20, marks: 85 },
+    { name: "Yaazh", age: 22, marks: 90 },
+    { name: "kevin", age: 23, marks: 88 }
+];
+console.log(students);
 
-let employees = {name: "Alice", age: 30, demo:()=>{
-    console.log("Hello "+this.name);} //this keyword will not work for arrow functions
-};
-employees.demo();
+students.forEach(student => {
+    console.log(`Name: ${student.name}, Age: ${student.age}`);
+});
+students.push({ name: "Arun", age: 21 });
+console.log(students);
+students.pop();
+console.log(students);
+students.map(student => {
+    return student.marks += 5;
+}); 
+console.log(students);
+students.filter(student => student.marks > 90);
+console.log(students);
