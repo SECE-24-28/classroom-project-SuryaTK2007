6125eb8ed95e183c9042e56a9cf38c8cea0973f4
Surya <suryatk2007@gmail.com>
2025-12-04 14:43:34 +0530

    used builtin functions for array in js

--- html/script.js
+++ html/script.js
@@ -1,14 +1,16 @@
-// difference between let and var in JavaScript?
-let x = 10;
-if (true) {
-    let x = 20; 
-    console.log(x); // Outputs: 20
-}
-console.log(x); // Outputs: 10
-
-var y = 10;
-if (true) {
-    var y = 20; 
-    console.log(y); // Outputs: 20
-}
-console.log(y); // Outputs: 20
+arr=["Surya","Vikram","Kiran","Ravi","Ajay"];
+console.log(arr);
+arr.push("Suresh");
+console.log(arr);
+arr.pop();
+console.log(arr);
+arr.shift(); // removes first element
+console.log(arr);
+arr.unshift("Mahesh"); // adds element at first position
+console.log(arr);
+// delete arr[2]; // deletes element but keeps index
+// console.log(arr);
+arr.splice(0,2); // removes 2 elements from index 0
+console.log(arr);
+let arr1=arr.slice(0,1); // creates new array from index 0 to 1 (not including 1)
+console.log(arr1);
