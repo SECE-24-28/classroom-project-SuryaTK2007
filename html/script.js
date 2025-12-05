68e8c9414acdb381da881f579f4a6754aec05680
Surya <suryatk2007@gmail.com>
2025-12-05 14:43:02 +0530

    Array and object destructuring in js

---
+++ html/script.js
@@ -0,0 +1,17 @@
+let arr=[10, 20, 30];
+let [a,b,c]=arr;
+console.log(a); 
+console.log(b); 
+console.log(c); 
+
+let obj={x:100, y:200, z:300};
+let {x, y, z}=obj;
+console.log(x); 
+console.log(y); 
+console.log(z);
+
+let obj2={p:1, q:2, r:3};
+let {p:m, q:n, r:o}=obj2;
+console.log(m); 
+console.log(n); 
+console.log(o);
