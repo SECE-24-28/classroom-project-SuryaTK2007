5f5519ec1425b9680b10fab39a2046e7e90891ad
Surya <suryatk2007@gmail.com>
2025-12-08 14:29:55 +0530

    implemented useState hook for counter

--- react/react-app/src/App.jsx
+++ react/react-app/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Temp from './Temp.jsx'
 
 function App() {
+  let [count, setCount] = useState(0)
   return (
     <>
+     <h1>Count is {count}</h1>
+     <button onClick={() => setCount((c) => c + 1)}>Increment</button>
+     <button onClick={() => setCount((c) => c - 1)}>Decrement</button>
      <Temp />
     </>
   )
