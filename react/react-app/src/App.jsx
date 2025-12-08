56972b53eb424433ea8cab8ecc4e06e6704833c1
Surya <suryatk2007@gmail.com>
2025-12-08 14:53:57 +0530

    implemented map in jsx

--- react/react-app/src/App.jsx
+++ react/react-app/src/App.jsx
@@ -3,13 +3,12 @@ import './App.css'
 import Temp from './Temp.jsx'
 
 function App() {
-  let [count, setCount] = useState(0)
+  let members=["Surya", "Saumyajit", "Yaazh"];
   return (
     <>
-     <h1>Count is {count}</h1>
-     <button onClick={() => setCount((c) => c + 1)}>Increment</button>
-     <button onClick={() => setCount((c) => c - 1)}>Decrement</button>
-     <Temp />
+     {
+        members.map((member)=>(<p>{member}</p>))
+     }
     </>
   )
 }
