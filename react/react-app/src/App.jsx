99ae7eb721c36410fb36803b7c5e357034adb1e0
Surya <suryatk2007@gmail.com>
2025-12-08 10:28:47 +0530

    using arrays, boolean values, null undefined and object and accessing object members in jsx

--- react/react-app/src/App.jsx
+++ react/react-app/src/App.jsx
@@ -5,10 +5,14 @@ import './App.css'
 
 function App() {
   let city="New York";
-
+  let nums=[1,2,3,4,5];
   return (
     <>
       <p>{city}</p>
+      <p>The array: {nums}</p>
+      <p>{JSON.stringify({name:"Surya",age:20})}</p>
+      {/* <p>{true}</p> same happens for false, null, undefined, it won't print anything */}
+      <p>{{name:"Surya",age:20}.name}</p>
     </>
   )
 }
