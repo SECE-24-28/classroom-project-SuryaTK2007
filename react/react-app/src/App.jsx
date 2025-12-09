580d3af2ad3ef73dfcae804e29a5306beeba11b5
Surya <suryatk2007@gmail.com>
2025-12-09 12:25:41 +0530

    Implemenetd search in the checklist for fee status

--- react/react-app/src/App.jsx
+++ react/react-app/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 function App() {
-  const originalList = [
+  const [originalList] = useState([
     { id: 1, sname: "surya", fee: false },
     { id: 2, sname: "hari", fee: false },
     { id: 3, sname: "kumar", fee: true }
-  ];
+  ]);
   const [list, setList] = useState(originalList);
   const [newName, setNewName] = useState("");
+  const [search, setSearch] = useState("");
   const handleDelete = (id) => {
     setList(list.filter(item => item.id !== id));
   };
@@ -32,10 +33,18 @@ function App() {
     setList([...list, newStudent]);
     setNewName("");
   };
+  const handleSearch = () => {
+    const filteredList = list.filter(item =>
+      item.sname.toLowerCase().includes(search.toLowerCase())
+    );
+    setList(filteredList);
+  };
   return (
     <>
       <h1>Students List</h1>
       <hr />
+      <input type='text' placeholder='Search here' value={search} onChange={(e)=>setSearch(e.target.value)}/><br /><br />
+      <button onClick={()=>handleSearch()}>Search</button><br /><br />
       <input 
         type="text" 
         placeholder="Enter student name"
