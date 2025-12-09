049166e0cf52e6db3ceaf32e63ba257f4c4fb2a2
Surya <suryatk2007@gmail.com>
2025-12-09 18:53:17 +0530

    implemented a jsx code for displaying students name

--- react/task/src/App.jsx
+++ react/task/src/App.jsx
@@ -1,9 +1,18 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [list, setList]=useState([{id:1,name:"surya",feeStatus:"false"},{id:2, name:"yaazh",feeStatus:"false"}])
   return (
     <>
-      
+      <h1>Students</h1>
+      <ul>
+          {list.map((li)=>
+          <li key={li.id}>
+            {li.name}
+          </li>
+          )}
+      </ul>
     </>
   )
 }
