cc64e53f2772ee4a08a6a624954d9fdc4482984c
Surya <suryatk2007@gmail.com>
2025-12-09 19:00:27 +0530

    implemented delete function for deleting student record

--- react/task/src/App.jsx
+++ react/task/src/App.jsx
@@ -2,14 +2,21 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [list, setList]=useState([{id:1,name:"surya",feeStatus:"false"},{id:2, name:"yaazh",feeStatus:"false"}])
+  const [list, setList]=useState([{id:1,name:"surya",fee:false},{id:2, name:"yaazh",fee:false}])
+  const handleDelete=(id)=>{
+    const newList=list.filter((li)=>li.id!=id)
+    setList(newList)
+  }
+
   return (
     <>
       <h1>Students</h1>
       <ul>
           {list.map((li)=>
           <li key={li.id}>
+            <input type='checkbox' checked={li.fee}/>
             {li.name}
+            <button onClick={()=>handleDelete(li.id)}>Delete</button>
           </li>
           )}
       </ul>
