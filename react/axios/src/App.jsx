49ee994f0dd43e677e828121efa5079ff5898d8d
Surya <suryatk2007@gmail.com>
2025-12-11 09:46:04 +0530

    fetched data from port 3500 /student and printed it using map

--- react/axios/src/App.jsx
+++ react/axios/src/App.jsx
@@ -1,12 +1,26 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import api from './api/Stu_api'
 
 function App() {
+  const [SList, setSList]=useState([])
+  useEffect(()=>{
+    const fetchData=async()=>{
+      const res=await api.get('/student')
+      setSList(res.data)
+    }
+    fetchData()
+  },[])
   return (
     <>
       <h1>Hello World</h1>
+      {
+        SList.map((stu) =>
+          <p key={stu.id}>{stu.id} - {stu.sname} - {stu.smark}</p>
+      )
+      }
     </>
   )
 }
