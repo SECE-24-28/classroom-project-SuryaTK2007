544e7433b41f5fd58248d0c40549cf9b45ddc8fb
Surya <suryatk2007@gmail.com>
2025-12-11 14:32:59 +0530

    implemented adding feedback feature using post method

--- react/feedback/src/component/AddPost.jsx
+++ react/feedback/src/component/AddPost.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import { useState , useContext} from 'react'
+import React, { use } from 'react'
+import { DataContext } from '../context/DataContext'
+import api from '../api/Post'
 
 const AddPost = () => {
+
+  const {posts, fetchData, title, setTitle, body, setBody}=useContext(DataContext)
+  const handleSubmit=async(e)=>{
+    e.preventDefault()
+    const newObj={
+      id:posts.length+1,
+      title:title,
+      body:body
+    }
+    await api.post("/feedback", newObj)
+    setTitle('')
+    setBody('')
+    fetchData()
+  }
+
   return (
     <div>
-      <form action="">
-        <input type='text' name='' id='' placeholder='Title'/><br/><br/>
-        <textarea name='' id='' placeholder='body'></textarea><br /><br />
+      <form onSubmit={handleSubmit}>
+        <input type='text' name='' id='' placeholder='Title' value={title} onChange={(e)=>setTitle(e.target.value)}/><br/><br/>
+        <textarea name='' id='' placeholder='body' value={body} onChange={(e)=>setBody(e.target.value)}/><br /><br />
         <button type='submit'>Save</button>
       </form>
     </div>
--- react/feedback/src/context/DataContext.jsx
+++ react/feedback/src/context/DataContext.jsx
@@ -5,11 +5,11 @@ const DataContext=createContext();
 
 export const DataProvider=({children})=>{
   const [posts, setPosts] = useState([])
-    useEffect(()=>{
-    const fetchData=async()=>{
-      const res=await api.get("/feedback");
-      setPosts(res.data)
-    }
+  const fetchData=async()=>{
+    const res=await api.get("/feedback");
+    setPosts(res.data)
+  }
+  useEffect(()=>{
     fetchData()
   },[])
   const [search, setSearch]=useState('');
@@ -18,8 +18,10 @@ export const DataProvider=({children})=>{
     const result=posts.filter((post)=>post.title.includes(search));
     setSearchResult(result);
   },[search])
+  const [title, setTitle]=useState('')
+  const [body, setBody]=useState('')
   return(
-    <DataContext.Provider value={{posts, search, setSearch, searchResult}}>
+    <DataContext.Provider value={{posts, setPosts, fetchData, search, setSearch, searchResult, title, setTitle, body, setBody}}>
         {children}
     </DataContext.Provider>
   )
