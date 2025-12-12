c8a0d7736d9c4f46e364213cd04943374d1b74a1
Surya <suryatk2007@gmail.com>
2025-12-12 10:01:59 +0530

    used route for the addPost component

--- react/feedback/src/App.jsx
+++ react/feedback/src/App.jsx
@@ -4,6 +4,8 @@ import Home from './component/Home'
 import { DataContext } from './context/DataContext';
 import Search from './component/Search';
 import AddPost from './component/AddPost';
+import { Link } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   const [activeTab, setActiveTab] = useState('home');
@@ -11,12 +13,22 @@ function App() {
   return (
     
     <div>
-      <AddPost/>
-      <div className='abc'>
-        <button id="homebtn" onClick={() => setActiveTab('home')}>Home</button>
-        <button id="searchbtn" onClick={() => setActiveTab('search')}>Search</button>
-      </div>
-      {activeTab === 'home' ? <Home /> : <Search />}
+      <ol>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/newpost'>NewPost</Link></li>
+      </ol>
+      <Routes>
+        <Route path='/' element={
+          <>
+            <div>
+              <button id="homebtn" onClick={() => setActiveTab('home')}>Home</button>
+              <button id="searchbtn" onClick={() => setActiveTab('search')}>Search</button>
+            </div><br />
+            {activeTab === 'home' ? <Home /> : <Search />}
+          </>
+        }></Route>
+        <Route path='newpost' element={<AddPost/>}></Route>
+      </Routes>
     </div>
   )
 }
